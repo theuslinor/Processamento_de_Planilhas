da2170546a947679b6e6d9598bc67a13bf7e0148
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-10-21 17:41:54 -0300

    first commit

---
+++ sheets-processor/src/main/java/com/application/sheets_processor/SheetsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.application.sheets_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetsProcessorApplication.class, args);
+	}
+
+}
---
+++ sheets-processor/src/test/java/com/application/sheets_processor/SheetsProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.sheets_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
